c63b0a71b6b283e98af6ec41289b88ddf0c3f588
Xu <Tony.T.Xu1@emc.com>
2018-07-12 15:49:44 +0800

    2018/7/12

--- 23.merge_k_sorted_lists.py
+++ 23.merge_k_sorted_lists.py
@@ -11,14 +11,17 @@ Input:
 ]
 Output: 1->1->2->3->4->4->5->6
 """
-
 # Definition for singly-linked list.
+
+from queue import PriorityQueue
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
-class Solution:
+
+class Solution1:
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
@@ -34,3 +37,31 @@ class Solution:
             point.next = ListNode(x)
             point = point.next
         return head.next
+
+class Solution2:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next=l2
