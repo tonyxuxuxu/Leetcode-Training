c085f6b29af5edef732e7792bce2f8211dcab75e
tianshixu <tonyxu0124@yahoo.com>
2018-08-08 22:06:43 +0800

    August 8, 2018

---
+++ 57.insert_interval.py
@@ -0,0 +1,37 @@
+"""
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+"""
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        start, end = newInterval.start, newInterval.end
+        left = list(filter(lambda x: x.end<start, intervals))
+        right = list(filter(lambda x: x.start>end, intervals))
+        if len(left) + len(right) != len(intervals):
+            start = min(start, intervals[len(left)].start)
+            end = max(end, intervals[-len(right) - 1].end)
+
+        return left + [Interval(start, end)] + right
