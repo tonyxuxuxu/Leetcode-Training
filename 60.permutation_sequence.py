6e3974f21f50256357c7dae6a53b3ec37f07a59d
tianshixu <tonyxu0124@yahoo.com>
2018-08-09 23:22:25 +0800

    August 9, 2018

--- 60.permutation_sequence.py
+++ 60.permutation_sequence.py
@@ -24,3 +24,15 @@ class Solution:
         :type k: int
         :rtype: str
         """
+        nums = map(str, range(1,10))
+        k -= 1
+        factorial = [1]*n
+        for i in range(1,n):
+            factorial[i] = factorial[i - 1] * i
+        res = []
+        for i in range(n):
+            index = k / factorial[n - i - 1]
+            res.append(nums[index])
+            nums.remove(nums[index])
+            k = k % factorial[n - 1 - i]
+        return ''.join(res)
