1a113cff2b997bfe79fce95437d72b30dcc272de
tianshixu <tonyxu0124@yahoo.com>
2018-08-18 12:54:08 +0800

    August 18, 2018

--- 78.subsets.py
+++ 78.subsets.py
@@ -35,6 +35,17 @@ class Solution(object):
             self.dfs(nums, i+1, res, path+[nums[i]])
 
 
+class Solution2(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        for num in sorted(nums):
+            res += [item + [num] for item in res]
+        return res
+
 if __name__ == '__main__':
     nums = [1, 2, 3]
     sol = Solution()
