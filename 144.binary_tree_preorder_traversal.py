45587b02598ddc45c2705ba8ca51c3f1db21eac9
tianshixu <tonyxu0124@yahoo.com>
2018-08-23 20:50:28 +0800

    August 23, 2018

---
+++ 144.binary_tree_preorder_traversal.py
@@ -0,0 +1,39 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, node, res):
+        if node:
+            res.append(node.val)
+            self.dfs(node.left, res)
+            self.dfs(node.right, res)
