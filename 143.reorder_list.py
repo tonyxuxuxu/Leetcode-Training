ea91529da05cf24ffd7c7a148144aa8e05247c59
tianshixu <tonyxu0124@yahoo.com>
2018-08-23 20:31:33 +0800

    August 23, 2018

---
+++ 143.reorder_list.py
@@ -0,0 +1,50 @@
+"""
+
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example 1:
+
+Given 1->2->3->4, reorder it to 1->4->2->3.
+Example 2:
+
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        # split
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        head1, head2 = head, slow.next
+        slow.next = None
+        # reverse
+        cur, pre = head2, None
+        while cur:
+            nex = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nex
+        # merge
+        cur1, cur2 = head1, pre
+        while cur2:
+            nex1, nex2 = cur1.next, cur2.next
+            cur1.next = cur2
+            cur2.next = nex1
+            cur1, cur2 = nex1, nex2
