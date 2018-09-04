0b6b3e338f66972accb7ad6901780f17282629e2
tianshixu <tonyxu0124@yahoo.com>
2018-09-04 20:19:07 +0800

    Sep 4, 2018

--- 153.find_minimum_in_rotated_sorted_array.py
+++ 153.find_minimum_in_rotated_sorted_array.py
@@ -36,3 +36,21 @@ class Solution(object):
             else:
                 right = mid
         return nums[mid]
+
+
+class Solution2(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) / 2
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid
+        return nums[lo]
+
+
